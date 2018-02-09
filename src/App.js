32e26bbfdad22a9c3582b5f6dea0e4c07aba2776
rendezvous3 <bojanj6m@gmail.com>
2018-02-08 23:30:27 -0500

    image component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import Images from './components/images'
 
 class App extends Component {
   render(){
-    return(<div>Whatever</div>)
+    return(<div>
+            <h5>React App</h5>
+            <hr />
+            <Images />
+          </div>)
   }
 }
 
